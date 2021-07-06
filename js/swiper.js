e660dd49be5923b5a671d1f5e5f7bcc1fccaed63
NamikoFujihara <86069645+NamikoFujihara@users.noreply.github.com>
2021-07-06 15:05:29 +0900

    sample
    sample

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('.swiper-container', {
+  speed: 5000,
+  slidesPerView: '3',
+  loop: true,
+  allowTouchMove: false, 
+  autoplay: {
+  delay: 0,
+  disableOnInteraction: false,
+  },
+})
